2ffadbc858900a94eabee03ef81f8bffe41c3f49
jekerr <jekerr96@gmail.com>
2019-11-10 16:35:00 +0700

    *

--- template/footer.php
+++ template/footer.php
@@ -1,6 +1,6 @@
 </main>
 <div class="to-top js-to-top hidden"></div>
-<? require "../partials/sticky-message.php" ?>
+<? require $_SERVER['DOCUMENT_ROOT'] . "/partials/sticky-message.php" ?>
 <script src="/js/bundle/<?= $pageType ?>.bundle.js?<?= filemtime($_SERVER['DOCUMENT_ROOT'] . "/js/bundle/". $pageType .".bundle.js") ?>"></script>
 </body>
 </html>
